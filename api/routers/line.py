ee2209d0566dd5f343e7bffa1cd0ce954a75dcd5
punksguitar <hanahanaogutaro@gmail.com>
2024-11-30 04:16:47 +0900

    解決済みゾロリ

--- api/routers/line.py
+++ api/routers/line.py
@@ -19,14 +19,14 @@ handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
     summary='LINE Message APIからのコールバック',
     description='ユーザーからメッセージを受信した際、LINE Message APIからこちらにリクエストが送られます。',
 )
-async def callback(request: Request):
-    body = await request.body()
+async def callback(request: Request, x_line_signature: str = Header(...)):
+    body = await request.body()  # リクエストボディを取得
 
     try:
-		# リクエストがLINEプラットフォームからのものかを検証する
-        handler.handle(body.decode("utf-8"))
-
+        # リクエストがLINEプラットフォームからのものかを検証する
+        handler.handle(body.decode("utf-8"), x_line_signature)
     except InvalidSignatureError:
         raise HTTPException(status_code=400, detail="InvalidSignatureError")
+
 
     return "OK"
--- main.py
+++ main.py
@@ -2,16 +2,10 @@ from fastapi import FastAPI
 # 追加
 # lineファイルをインポート
 import os
-from api.routers import line
+from api.routers.line import router
 
 app = FastAPI()
 
 # 追加
 # lineファイルで定義したルートをアプリケーションに適用
-app.include_router(line.router)
-
-@app.get("/hello")
-async def hello():
-    print(f"CHANNEL_SECRET: {os.environ.get('CHANNEL_SECRET')}")
-
-    return {"message": "hello world!"}
+app.include_router(router)
