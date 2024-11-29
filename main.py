3cdee486b3933157e5822a58a65138e8761a290a
punksguitar <hanahanaogutaro@gmail.com>
2024-11-30 00:25:58 +0900

    一旦リセット

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 # 追加
 # lineファイルをインポート
-from .routers import line
+# from .routers import line
 
 app = FastAPI()
 
 # 追加
 # lineファイルで定義したルートをアプリケーションに適用
-app.include_router(line.router)
+# app.include_router(line.router)
 
 @app.get("/hello")
 async def hello():
