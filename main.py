43bd27b4a6fd92ecac1381d1ef8e88d568cd42c2
punksguitar <hanahanaogutaro@gmail.com>
2024-11-29 23:47:28 +0900

    routersフォルダとメインの接続

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
+
+#routersの接続
+from routers import line
 app = FastAPI()
+# lineファイルで定義したルートをアプリケーションに適用
+app.include_router(line.router)
+
+
 @app.get("/hello")
 async def hello():
     return{"message":"hello world!"}
